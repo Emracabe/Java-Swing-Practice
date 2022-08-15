690ff9650d792920fbf1de1ae14f6093c935dcc0
iMAC 16 MAC Lab <imac16maclab@net-130-25-3-148.cust.vodafonedsl.it>
2022-08-15 13:29:14 +0800

    Rename some files and add some documentation(comments) None exit

--- src/Frames.java
+++ src/Frames.java
@@ -1,5 +1,9 @@
 public class Frames {
     public static void main(String[] args) {
-        new MyFrame();
+        // Creates a jframe component
+        // JFrameChild class extends the JFrame class
+        // To understand this code of line visit JFrameChild.java
+        // And know the OOP Concept, especially inheritance
+        JFrameChild jframe = new JFrameChild();
     }
 }
