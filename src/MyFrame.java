32b22ba097de862de50084b132725695e951b7c7
iMAC 16 MAC Lab <imac16maclab@net-130-25-3-148.cust.vodafonedsl.it>
2022-08-15 13:13:20 +0800

    Modified the filenames

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,14 +4,14 @@ import java.awt.*;
 public class MyFrame extends JFrame {
 
     MyFrame() {
-        this.setTitle("Emmar's JFrame"); // sets title of frame
+        this.setTitle("Emmar's JFrame"); // sets title of Frames
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
-        this.setResizable(false); // prevent frame from being resized
-        this.setSize(1000, 800); // sets the x-dimension and y-dimension of frame
-        this.setVisible(true); // make frame visible
+        this.setResizable(false); // prevent Frames from being resized
+        this.setSize(1000, 800); // sets the x-dimension and y-dimension of Frames
+        this.setVisible(true); // make Frames visible
 
         ImageIcon image = new ImageIcon("logo.jpeg"); // create an ImageIcon
-        this.setIconImage(image.getImage()); // change icon of frame
-        this.getContentPane().setBackground(new Color(0x502BB4)); // change background color of frame
+        this.setIconImage(image.getImage()); // change icon of Frames
+        this.getContentPane().setBackground(new Color(0x502BB4)); // change background color of Frames
     }
 }
